ad45bac2f77ab42d5ce032784c708c304b7c255b
krendeleno <krendeleno@gmail.com>
2020-06-11 16:10:03 +0800

    Маяковский в эфире!

--- JS/Author.js
+++ JS/Author.js
@@ -13,7 +13,7 @@ $(function () {
 
     // Выпадающее верхнее меню
     $(window).scroll(function(event) {
-        if ($(this).scrollTop () > $(window ).height() * 0.1) {
+        if ($(this).scrollTop () > $(window ).height() * 0.1 && $(window ).width() > 600) {
             $(".scroll_menu").slideDown();
             return false;
         }
@@ -21,6 +21,43 @@ $(function () {
             $(".scroll_menu").slideUp();
         }
     });
+
+    function  transition () {
+        $(".column1").css("display", "none");
+        $(".column2").css("display", "flex");
+        $(".column1").removeClass("animate__fadeOutLeft");
+    }
+
+    function reverse_transition () {
+        $(".column2").css("display", "none");
+        $(".column1").css("display", "block");
+        $(".column2").removeClass("animate__fadeOutLeft");
+    }
+
+    function crutch() {
+        $(".column1").removeClass("animate__fadeInLeft");
+    }
+
+    function trip() {
+        if ($(".column1").css("display") != "none") {
+            $(".column1").addClass("animate__fadeOutLeft");
+            $(".column2").addClass("animate__fadeInRight");
+            setTimeout(transition, 500);
+            $(".column2").removeClass("animate__fadeInRight");
+            $(".back").css("transform", "rotate(0deg)").css("background-image",'url("Literature/back.png")');
+            $(".hamburger").css("background-image",'url("Music/burger.png")');
+            $(".column1").addClass("animate__fadeInLeft");
+        } else {
+            $(".column2").addClass("animate__fadeOutLeft");
+            setTimeout(reverse_transition, 500);
+            $(".back").css("transform", "rotate(180deg)").css("background-image", 'url("Literature/white_back.png")');
+            $(".hamburger").css("background-image",'url("Music/white_burger.png")');
+            setTimeout(crutch, 1000);
+        }
+    }
+
+        $(".back").bind("click", trip);
+        $("li").bind("click", trip);
 
     // //Фикс левой фотки
     // $(window).scroll(function(event) {
